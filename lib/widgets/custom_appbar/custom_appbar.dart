36ae325cf4d10fea789001010590211f69b7b578
Hasibul Hossain <hasibulhossain52@gmail.com>
2022-04-27 16:01:14 +0600

    update: added title on custom appbar

--- lib/widgets/custom_appbar/custom_appbar.dart
+++ lib/widgets/custom_appbar/custom_appbar.dart
@@ -1,33 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/src/size_extension.dart';
 import 'package:nft_marketplace_concept/widgets/applogo/applogo.dart';
-
 
 class CustomAppBar extends StatelessWidget {
   final Widget? leading;
   final Widget? trailing;
   final bool? isBackEnabled;
-  final void Function()? leadingCb; // call back function for leading widget onClick.
+  final String? title;
+  final void Function()?
+      leadingCb; // call back function for leading widget onClick.
 
-  const CustomAppBar({
-    Key? key,
-    this.leading,
-    this.trailing,
-    this.isBackEnabled,
-    this.leadingCb,
-  }) : super(key: key);
+  const CustomAppBar(
+      {Key? key,
+      this.leading,
+      this.trailing,
+      this.isBackEnabled,
+      this.leadingCb,
+      this.title})
+      : super(key: key);
 
   List<Widget> appBar() {
-    if(leading == null && isBackEnabled != null && isBackEnabled! && leadingCb != null) {
+    if (leading == null &&
+        isBackEnabled != null &&
+        isBackEnabled! &&
+        leadingCb != null) {
       return [
-        IconButton(onPressed: leadingCb, icon: const Icon(Icons.keyboard_backspace_outlined)),
-        if(trailing != null) trailing!,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            IconButton(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(top: 8, right: 8, bottom: 8, left: 0),
+              onPressed: leadingCb,
+              icon: const Icon(Icons.keyboard_backspace_outlined),
+            ),
+            if(title != null) Text(title!, style: TextStyle(fontSize: 20.sp),)
+          ],
+        ),
+        if (trailing != null) trailing!,
       ];
     }
 
-    if(isBackEnabled == null || !isBackEnabled! && trailing != null) {
+    if (isBackEnabled == null || !isBackEnabled! && trailing != null) {
       return [
         const AppLogo(),
-        if(trailing != null) trailing!,
+        if (trailing != null) trailing!,
       ];
     }
 
